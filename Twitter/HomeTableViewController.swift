5a87406b34ae73bedc6da1c1748ccc713b6933be
Yazan Arafeh <yazanarafeh24@gmail.com>
2020-03-08 20:55:00 -0700

    Added favorite and retweet actions

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -82,6 +82,10 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.tweetID = tweetArray[indexPath.row]["id"] as! Int
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.setRetweet(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
         return cell
     }
     
--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -9,11 +9,64 @@
 import UIKit
 
 class TweetCell: UITableViewCell {
-
     
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
+    
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
+    
+    var tweetID: Int = -1
+    var favorited: Bool = false
+    var retweeted: Bool = false
+    
+    @IBAction func favoriteTweet(_ sender: UIButton) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetID: tweetID, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succed: \(error)")
+            })
+        }
+        else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetID: tweetID, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("Unfavorite did not succeed : \(error)")
+            })
+        }
+    }
+    
+    func setFavorite(_ isFavorited: Bool) {
+        favorited = isFavorited
+        if (favorited) {
+            favButton.setImage(UIImage(named: "Favor-icon-red"), for: .normal)
+        }
+        else {
+            favButton.setImage(UIImage(named: "favor-icon"), for: .normal)
+        }
+    }
+    
+    @IBAction func retweet(_ sender: UIButton) {
+        TwitterAPICaller.client?.retweet(tweetID: tweetID, success: {
+            self.setRetweet(true)
+        }, failure: { (error) in
+            print("Error retweeting: \(error)")
+        })
+    }
+    
+    func setRetweet(_ isRetweeted: Bool) {
+        if (isRetweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: .normal)
+            retweetButton.isEnabled = false
+        }
+        else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: .normal)
+            retweetButton.isEnabled = true
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
